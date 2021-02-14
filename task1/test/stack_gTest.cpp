d324c55360f1817f3f0af431c5a2b6d646c9960f
Mikhail Didur <mihasyaterminator@gmail.com>
2021-02-14 18:27:07 +0200

    Added google tests

--- task1/test/stack_gTest.cpp
+++ task1/test/stack_gTest.cpp
@@ -1,2 +1,12 @@
 #include "stack.h"
+#include "gtest/gtest.h"
+#include <stdio.h>
 
+int main(int argc, char** argv)
+{
+	puts("Google tests...");
+
+	testing::InitGoogleTest(&argc, argv);
+
+	return RUN_ALL_TESTS();
+}
