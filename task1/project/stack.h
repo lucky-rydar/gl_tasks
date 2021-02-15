82884fa315f7e5014d5be109454ef036b408cca5
Mikhail Didur <mihasyaterminator@gmail.com>
2021-02-15 08:49:57 +0200

    Realized class' methods and added a few tests

--- task1/project/stack.h
+++ task1/project/stack.h
@@ -1,4 +1,5 @@
 #include <vector>
+#include <stdexcept>
 
 namespace collections
 {
@@ -6,14 +7,73 @@ namespace collections
 	class stack
 	{
 	public:
-		stack();
-		stack(size_t size); // creating stack with size
-		void push(T elem); // add element on stack
-		T pop(); // get top element
-		size_t capacity(); // amount of elements in stack
-		size_t size(); // maxi amount of elements
+		// the task requires *create* method, but I hope that is only for C
+		// so I just made constructor
+
+		// creating stack with size
+		stack(size_t size) : elems()
+		{
+			if (size == 0)
+				throw std::logic_error("Size of stack can not be zero");
+			
+			elems.reserve(size);
+		}
+
+		// add element on stack
+		void push(T elem) 
+		{
+			if (is_full())
+				throw std::out_of_range("Stack is full");
+
+			elems.push_back(elem);
+		}
+		
+		// get top element
+		T pop() 
+		{
+			if (is_empty())
+				throw std::out_of_range("Stack is empty");
+
+			T temp = elems[elems.size()-1];
+			elems.pop_back();
+			
+			return temp;
+		}
+		
+		// amount of elements in stack
+		size_t capacity()
+		{
+			return elems.size();
+		}
+		
+		// max amount of elements
+		size_t size()
+		{
+			return elems.capacity();
+		}
+		
+		//just resize
+		void resize(size_t new_size)
+		{
+			if (new_size < elems.size())
+				elems.resize(new_size);
+			else if (new_size > elems.size())
+				elems.reserve(new_size);
+		}
 
 	private:
-		std::vector<T> data;
+		//this method tells if stack is full or not
+		bool is_full()
+		{
+			return (elems.size() == elems.capacity());
+		}
+
+		bool is_empty()
+		{
+			return elems.empty();
+		}
+
+		std::vector<T> elems;
 	};
+	
 }
--- task1/test/stack_gTest.cpp
+++ task1/test/stack_gTest.cpp
@@ -2,6 +2,63 @@
 #include "gtest/gtest.h"
 #include <stdio.h>
 
+using namespace collections;
+
+TEST(stack, stack)
+{
+	ASSERT_THROW(stack<int> s(0), std::logic_error);
+	
+	stack<int> a(2);
+	ASSERT_EQ(a.capacity(), 0);
+	ASSERT_EQ(a.size(), 2);
+}
+
+TEST(stack, push)
+{
+	stack<int> s(2);
+	s.push(1);
+	
+	ASSERT_EQ(s.capacity(), 1);
+
+	s.push(1);
+	ASSERT_THROW(s.push(1), std::out_of_range);
+}
+
+TEST(stack, pop)
+{
+	stack<int> s(1);
+	
+	s.push(1337);
+	ASSERT_EQ(1337, s.pop());
+
+	ASSERT_THROW(s.pop(), std::out_of_range);
+
+}
+
+TEST(stack, resize1)
+{
+	stack<int> s(4);
+	for (int i = 0; i < 4; i++)
+		s.push(i);
+
+	s.resize(3);
+	ASSERT_EQ(s.pop(), 2);
+
+	
+	s.resize(8);
+	s.push(228);
+	ASSERT_EQ(s.pop(), 228);
+}
+
+TEST(stack, resize2)
+{
+	stack<int> s(1);
+	s.push(1);
+	s.resize(2);
+	ASSERT_NO_THROW(s.push(1337));
+	ASSERT_EQ(s.pop(), 1337);
+}
+
 int main(int argc, char** argv)
 {
 	puts("Google tests...");
