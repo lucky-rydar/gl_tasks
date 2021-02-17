25ed381ac98a51a607a1165cc8cb0f90402d09b7
Mikhail Didur <mihasyaterminator@gmail.com>
2021-02-17 11:36:32 +0200

    Updated gitignore file Realized main for first task

--- task1/project/main.cpp
+++ task1/project/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <string>
 #include "stack.h"
 
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
-	
+    if(argc != 2)
+    {
+        cout << "Invalid input" << endl;
+        return -1;
+    }
+
+    string input(argv[1]);
+    if(input == "help")
+    {
+        cout << "Usage: ./main help"  << endl \
+            << "To be honest I have no idea what should be here" << endl;
+
+    }
 }
